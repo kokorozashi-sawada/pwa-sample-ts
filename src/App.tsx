927da6f724b8fa54f4d181917b52abdac5204ff0
kokoroashi-sawada <takuma.sawada@kokorozashi-japan.co.jp>
2023-10-26 19:18:23 +0900

    アップデートテスト

--- src/App.tsx
+++ src/App.tsx
@@ -151,15 +151,6 @@ function App() {
               アップロード
             </button>
           </div>
-          <div className="mb-10">
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={uploadData}
-            >
-              アップロード
-            </button>
-          </div>
         </div>
       </div>
       <ul className="divide-y divide-gray-100">
